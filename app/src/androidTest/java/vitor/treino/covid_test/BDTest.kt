fba1ff6921819ae85bb329bd16ffae88bdddfe22
segur <seguro17@gmail.com>
2021-06-10 23:25:02 +0100

    A tabela Hospital está a comunicar com a aplicação :D

--- app/src/androidTest/java/vitor/treino/covid_test/BDTest.kt
+++ app/src/androidTest/java/vitor/treino/covid_test/BDTest.kt
@@ -10,11 +10,6 @@ import org.junit.runner.RunWith
 import org.junit.Assert.*
 import org.junit.Before
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class BDTest {
     private fun getAppContext() = InstrumentationRegistry.getInstrumentation().targetContext
